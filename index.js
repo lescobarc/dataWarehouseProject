06e9698b6925c39466e4efdfc8d14b6a8dfff8fe
lauraescobarc <66136705+lescobarc@users.noreply.github.com>
2020-09-28 12:18:17 -0500

    importContacts

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+
+
+let iconFilter = document.getElementById('iconFilter');
+let contactsFilter = document.getElementById('contactsFilter')
+ iconFilter.addEventListener('click', ()=>{
+    contactsFilter.classList.toggle('hidden')
+});
+
+
 let exportButton = document.getElementById('exportButton');
 exportButton.addEventListener('click', () => {
     document.getElementById('exportContactsOptions').classList.toggle('exportContactsOptions')
