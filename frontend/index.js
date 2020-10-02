e9321a875aabe5a567693e7b37e7b999d58e98e1
lauraescobarc <66136705+lescobarc@users.noreply.github.com>
2020-10-02 13:39:50 -0500

    contacts buttons function

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,6 @@
 
 
+// 2. Drop Down Filter
 let iconFilter = document.getElementById('iconFilter');
 let contactsFilter = document.getElementById('contactsFilter')
  iconFilter.addEventListener('click', ()=>{
@@ -7,7 +8,49 @@ let contactsFilter = document.getElementById('contactsFilter')
 });
 
 
+//7. Export Contacts 
 let exportButton = document.getElementById('exportButton');
+let exportContactsOptions = document.getElementById('exportContactsOptions')
 exportButton.addEventListener('click', () => {
-    document.getElementById('exportContactsOptions').classList.toggle('exportContactsOptions')
+    exportContactsOptions.classList.toggle('exportContactsOptions')
 });
+
+//8. Import Contacts
+let iconImport = document.getElementById('iconImport');
+let importContactsSection = document.getElementById('importContactsSection');
+iconImport.addEventListener('click', ()=>{
+    importContactsSection.classList.toggle('hidden');
+});
+
+let cancelButtonImport = document.getElementById('cancelButtonImport');
+cancelButtonImport.addEventListener('click', ()=>{
+    importContactsSection.classList.toggle('hidden')
+});
+
+//9. Delete Contacts
+let contactId = 1;
+let iconTrashDelete = document.getElementById(`iconTrashDelete${contactId}`);
+let deleteContactsSection = document.getElementById('deleteContactsSection');
+iconTrashDelete.addEventListener('click', ()=>{
+    deleteContactsSection.classList.toggle('hidden')
+});
+
+let cancelButtonDelete = document.getElementById('cancelButtonDelete');
+cancelButtonDelete.addEventListener('click', ()=>{
+    deleteContactsSection.classList.toggle('hidden')
+});
+
+//10. New Contact
+let iconAdd = document.getElementById('iconAdd');
+let newContactSection = document.getElementById('newContactSection');
+iconAdd.addEventListener('click', ()=>{
+    newContactSection.classList.toggle('hidden')
+})
+
+let cancelButtonNewContact = document.getElementById('cancelButtonNewContact');
+cancelButtonNewContact.addEventListener('click', ()=>{
+    newContactSection.classList.toggle('hidden')
+})
+
+
+
