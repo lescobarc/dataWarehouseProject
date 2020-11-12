44da185eba73bd99f4377138c44d9e8f0e57abe2
lauraescobarc <66136705+lescobarc@users.noreply.github.com>
2020-11-11 19:03:24 -0500

    search contact

--- frontend/js/contacts.js
+++ frontend/js/contacts.js
@@ -3,6 +3,7 @@ let token = localStorage.token;
 //Table 
 let table = document.querySelector('#contactsTable tbody');
 let contactsSection = document.getElementById('contactsSection');
+let iconSearch = document.getElementById('iconSearch')
 
 //CONTACTS
 
@@ -53,6 +54,9 @@ let buttonAddChannelUp = document.getElementById('buttonAddChannelUp');
 let deleteContactsSection = document.getElementById('deleteContactsSection');
 let cancelButtonDeleteContact = document.getElementById('cancelButtonDeleteContact');
 let deleteButtonDeleteContact = document.getElementById('deleteButtonDeleteContact')
+
+//search
+let inputSearchContact = document.getElementById('contactsInput')
 
 //1. get contacts
 function getContacts() {
@@ -751,4 +755,53 @@ async function deleteContact(id) {
 
 //SEARCH CONTACT
 
-  
+function doSearch()
+{
+    
+    const searchText = inputSearchContact.value.toLowerCase();
+    let total = 0;
+
+    // Recorremos todas las filas con contenido de la tabla
+    for (let i = 1; i < table.rows.length; i++) {
+        // Si el td tiene la clase "noSearch" no se busca en su cntenido
+        if (table.rows[i].classList.contains("noSearch")) {
+            continue;
+        }
+
+        let found = false;
+        const cellsOfRow = table.rows[i].getElementsByTagName('td');
+        // Recorremos todas las celdas
+        for (let j = 0; j < cellsOfRow.length && !found; j++) {
+            const compareWith = cellsOfRow[j].innerHTML.toLowerCase();
+            // Buscamos el texto en el contenido de la celda
+            if (searchText.length == 0 || compareWith.indexOf(searchText) > -1) {
+                found = true;
+                total++;
+            }
+        }
+        if (found) {
+            table.rows[i].style.display = '';
+        } else {
+            // si no ha encontrado ninguna coincidencia, esconde la
+            // fila de la tabla
+            table.rows[i].style.display = 'none';
+        }
+    }
+
+    // mostramos las coincidencias
+    const lastTR=table.rows[table.rows.length-1];
+    const td=lastTR.querySelector("td");
+    lastTR.classList.remove("hide", "red");
+    if (searchText == "") {
+        lastTR.classList.add("hide");
+    } else if (total) {
+        td.innerHTML="Se ha encontrado "+total+" coincidencia"+((total>1)?"s":"");
+    } else {
+        lastTR.classList.add("red");
+        td.innerHTML="No se han encontrado coincidencias";
+    }
+}
+
+iconSearch.addEventListener('click', () =>{
+    doSearch();
+})
